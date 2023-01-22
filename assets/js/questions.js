f211440f94d142d1d27346788dfa0c93e4154ae8
Jonathan Davis <trunten@btinternet.com>
2023-01-22 13:31:26 +0000

    Added questions

---
+++ assets/js/questions.js
@@ -0,0 +1,33 @@
+const questions = [
+    {
+        question: "Commonly used data types DO NOT include.",
+        choices: ["strings","booleans","alerts","numbers"],
+        answer: "alerts",
+    },
+    {
+        question: "The condition in an if else statement is enclosed within _____.",
+        choices: ["quotes","curly braces","parentheses","square brackets"],
+        answer: "parentheses",
+    },
+    {
+        question: "Arrays in JavaScriot can be used to store _______.",
+        choices: ["numbers & strings","other arrays","objects","all of the above"],
+        answer: "all of the above",
+    },
+    {
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        choices: ["commas","curly braces","quotes","parentheses"],
+        answer: "quotes",
+    },
+    {
+        question: "A very userul tool used during development and debugging for printing content to the debugger is _________.",
+        choices: ["JavaScript","terminal/bash","for loops","console.log"],
+        answer: "console.log",
+    },
+    {
+        question: "Values can be save to local storage using ________.",
+        choices: ["getValue","setValue","addItem","getItem"],
+        answer: "getItem",
+    },
+    
+];
